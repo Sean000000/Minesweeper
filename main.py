7eea80075bcfb314df3fef22cf9ef1b6acf56d9c
Sean000000 <38412553+Sean000000@users.noreply.github.com>
2018-05-31 23:40:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.messagebox
 from Minesweeper import Minesweeper
 
 # Game settings.
@@ -28,27 +29,24 @@ class   GUI(tkinter.Frame):
 
         # Add menu to window.
         menubar = tkinter.Menu(self.master)
-        menubar.add_cascade(label='Reset', command=self.onResetClick)
+        menubar.add_cascade(label='new game', command=self.onResetClick)
         self.master['menu'] = menubar
 
         # Add buttons to window.
-        self.button_list = list()
-        for y in range(GAME_SETTING['y']):
-            button_row = list()
-            for x in range(GAME_SETTING['x']):
-                button = tkinter.Button(self.master)
-                button['text'] = ''
+        self.button_list = [[tkinter.Button(self.master) for x in range(GAME_SETTING['x'])] for y in range(GAME_SETTING['y'])]
+        for y,button_row in enumerate(self.button_list):
+            for x,button in enumerate(button_row):
+                button['text'] = '(' + str(x) + ',' + str(y) + ')'
                 button.grid(column=x, row=y)
                 button['height'] = 1
                 button['width'] = 2
-                button.bind('<Button-1>', lambda event, axis={'x':x,'y':y}:self.onLeftClick(axis))
+               #button.bind('<Button-1>', lambda event, axis={'x':x,'y':y}:self.onLeftClick(axis))
+                button['command'] = lambda axis={'x':x,'y':y}:self.onLeftClick(axis)
                 button.bind('<Button-3>', lambda event, axis={'x':x,'y':y}:self.onRightClick(axis))
-                button_row.append(button)
-            self.button_list.append(button_row)
 
         # Create a game instance and reset it.
         self.game = Minesweeper()
-        self.game.newGame(GAME_SETTING['x'], GAME_SETTING['y'], GAME_SETTING['mines'])
+        self.game.newGame(size_x=GAME_SETTING['x'], size_y=GAME_SETTING['y'], mine_number=GAME_SETTING['mines'])
         self.nextState()
 
     def onResetClick(self):
@@ -58,7 +56,7 @@ class   GUI(tkinter.Frame):
         '''
         print("Reset click!")
         
-        self.game.reset()
+        self.game.newGame(size_x=GAME_SETTING['x'], size_y=GAME_SETTING['y'], mine_number=GAME_SETTING['mines'])
         self.nextState()
 
     def onLeftClick(self, axis):
@@ -69,7 +67,7 @@ class   GUI(tkinter.Frame):
         '''
        #print("Button(", axis['x'], ',', axis['y'], ') be left clicked!', sep='')
         
-        self.game.open(axis['y'],axis['x'])
+        self.game.open(x=axis['x'], y=axis['y'])
         self.nextState()
 
     def onRightClick(self, axis):
@@ -80,7 +78,7 @@ class   GUI(tkinter.Frame):
         '''
        #print("Button(", axis['x'], ',', axis['y'], ') be right clicked!', sep='')
 
-        self.game.setFlag(axis['x'], axis['y'])
+        self.game.flag(x=axis['x'], y=axis['y'])
         self.nextState()
 
     def nextState(self):
@@ -88,15 +86,28 @@ class   GUI(tkinter.Frame):
         nextState()
             None.
         '''
+        unknown_count = 0
+        boom_count = 0
         game_map = self.game.getMap()
-        for y in range(GAME_SETTING['y']):
-            for x in range(GAME_SETTING['x']):
-                if game_map[y][x] == 'unknown':
+        for y,game_map_row in enumerate(game_map):
+            for x,game_map_node in enumerate(game_map_row):
+                self.button_list[y][x]['text'] = SYMBOL_LIST[game_map[y][x]]
+
+                if game_map_node == 'unknown':
                     self.button_list[y][x]['relief'] = tkinter.RAISED
+                    unknown_count = unknown_count + 1
+                elif game_map_node == 'flag':
+                    self.button_list[y][x]['relief'] = tkinter.RAISED
+                elif game_map_node == 9:
+                    tkinter.messagebox.showinfo('Boom', '_:(´°ω°`」 ∠):_')
+                    boom_count = boom_count + 1
                 else:
                     self.button_list[y][x]['relief'] = tkinter.SUNKEN
 
-                self.button_list[x][y]['text'] = SYMBOL_LIST[game_map[y][x]]
+        if unknown_count==0 and boom_count==0:
+            print('uc:',unknown_count)
+            tkinter.messagebox.showinfo('Win', 'Y(9q9)Y')
+                
                 
        #self.button_list[axis['x']][axis['y']]['text'] = SYMBOL_LIST['mine']
        #self.button_list[axis['x']][axis['y']]['text'] = SYMBOL_LIST['flag']
